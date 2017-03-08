a8618b7ac3bcbe23de26e1e582da7401f8205814
Laurent Bassin <laurent@bassin.info>
2017-03-08 22:43:01 +0000

    CSS Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+        <form class="col offset-s3 s6">
+            <div class="input-field">
+                <input id="search" type="search" required>
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+            </div>
+        </form>
+    </div>
+    
+    <div class="row">
+        <div class="col s3">
+            <div class="row user-block">
+                <div class="col s10 offset-s1">
+                    <h3 class="center-align">Laurent</h3>
+                    <img src="http://materializecss.com/images/yuna.jpg" class="circle center-block" alt="">
+                </div>
+            </div>
+    
+            <div class="row background-pink fake-height">
+                <div class="col s10 offset-s1">
+                    <h3 class="center-align">Suggestions</h3>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col s8 offset-s1 background-green">
+            <div class="row">
+                <form class="col s10 offset-s1 margin-top">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="textarea1" class="materialize-textarea"></textarea>
+                            <label for="textarea1">Quoi de neuf ?</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s3 offset-s9">
+                            <button class="btn waves-effect waves-light" type="submit">Submit
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <!-- Posts -->
+            <div class="row">
+                <div class="col s2">
+                    <img src="http://materializecss.com/images/yuna.jpg" class="center-block" alt="">
+                </div>
+                <div class="col s10">
+                    <p>
+                        Fabio - 8 Mars à 23h35 :
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab adipisci animi consectetur dolorem eius enim est maiores nemo numquam officia pariatur, quam quis quo, quos recusandae reiciendis veniam voluptatem.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s2">
+                    <img src="http://materializecss.com/images/yuna.jpg" class="center-block" alt="">
+                </div>
+                <div class="col s10">
+                    <p>
+                        Fabio - 8 Mars à 23h35 :
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab adipisci animi consectetur dolorem eius enim est maiores nemo numquam officia pariatur, quam quis quo, quos recusandae reiciendis veniam voluptatem.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s2">
+                    <img src="http://materializecss.com/images/yuna.jpg" class="center-block" alt="">
+                </div>
+                <div class="col s10">
+                    <p>
+                        Fabio - 8 Mars à 23h35 :
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab adipisci animi consectetur dolorem eius enim est maiores nemo numquam officia pariatur, quam quis quo, quos recusandae reiciendis veniam voluptatem.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -11,8 +11,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     
     <!-- Scripts -->
     <script>
@@ -22,63 +23,26 @@
     </script>
 </head>
 <body>
-
-    <nav class="nav-border">
-        <div class="nav-wrapper">
-            <div class="container">
-                
-                <div class="row">
-                    <div class="col s2">
-                        <a href="#" class="brand-logo">Logo</a>
-                        <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
-                    </div>
-                    
-                    <div class="col s8 hide-on-med-and-down">
-                        <form>
-                            <div class="input-field">
-                                <input id="search" type="search" required>
-                                <label class="label-icon" for="search"><i class="material-icons">search</i>
-                                </label>
-                                <i class="material-icons closed">close</i>
-                            </div>
-                        </form>
-                    </div>
-                    
-                    <div class="col s2 hide-on-med-and-down">
-                        <a class='dropdown-button' href='#' data-activates='dropdown-menu'>Drop Me!</a>
-                        <ul id='dropdown-menu' class='dropdown-content'>
-                            <li><a href="#!">one</a></li>
-                            <li><a href="#!">two</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#!">three</a></li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <ul class="side-nav" id="mobile-menu">
-                    <li><a href="#"><i class="material-icons">perm_identity</i></a>
-                    </li>
-                </ul>
-                <ul id="slide-out" class="side-nav">
-                    <li><div class="userView">
-                            <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-                            <a href="#!name"><span class="white-text name">John Doe</span></a>
-                            <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-                        </div></li>
-                    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-                    <li><a href="#!">Second Link</a></li>
-                    <li><div class="divider"></div></li>
-                    <li><a class="subheader">Subheader</a></li>
-                    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-                </ul>
-                <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+    <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="{{ url('home') }}" class="brand-logo">Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
             
-            </div>
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
         </div>
     </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
     
     <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="{{ asset('js/materialize.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
